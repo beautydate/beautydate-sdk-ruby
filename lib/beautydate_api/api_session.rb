dd5665d9cbd3e239a11520d7c212b454e0c3419f
Leonardo Saraiva <vyper@maneh.org>
2017-01-25 11:16:26 -0200

    Improve code for api session

--- lib/beautydate_api/api_session.rb
+++ lib/beautydate_api/api_session.rb
@@ -2,32 +2,44 @@ module BeautydateApi
   class APISession
 
     def authenticate(authorization, email, password)
-      request = {
-        method: 'POST',
-        url: "#{BeautydateApi.base_uri}sessions",
-        timeout: 30,
+      request = meta
+        .merge(headers(authorization))
+        .merge(payload(email, password))
+
+      result = JSON.parse(RestClient::Request.execute request)
+      @token_key = result.dig('data', 'attributes', 'token')
+      @expires_at = result.dig('data', 'attributes', 'expires_at')
+
+      authenticated?
+    rescue => e
+      raise AuthenticationException
+    end
+
+    def meta
+      { method: 'POST', url: "#{BeautydateApi.base_uri}sessions", timeout: 30 }
+    end
+
+    def headers(authorization)
+      {
         headers: {
           user_agent: "BeautyDate/#{BeautydateApi::VERSION}; Ruby Client",
           content_type: 'application/vnd.api+json',
           authorization: authorization
-        },
+        }
+      }
+    end
+
+    def payload(email, password)
+      {
         payload: {
           data: {
             type: 'sessions',
             attributes: {
-              provider: 'b2beauty',
-              email: email,
-              password: password
+              provider: 'b2beauty', email: email, password: password
             }
           }
         }.to_json
       }
-      result = JSON.parse(RestClient::Request.execute request)
-      @token_key = result['data']['attributes']['token']
-      @expires_at = result['data']['attributes']['expires_at']
-      true
-    rescue => e
-      raise AuthenticationException
     end
 
     def authenticated?
