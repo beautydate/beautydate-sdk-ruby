2f7e3668afc810a3106ec138a2432608ab7b4495
Tiago Guedes <tiagopog@gmail.com>
2017-08-14 09:33:05 -0300

    Implement BeautydateApi::Core::Object#{chages,changed,*_was}

--- lib/beautydate_api/core/object.rb
+++ lib/beautydate_api/core/object.rb
@@ -1,74 +1,35 @@
 module BeautydateApi
   module Core
     class Object
-      attr_accessor :id, :attributes, :errors
+      TOP_LEVEL_DATA = %w(id type attributes relationships).freeze
 
-      def initialize(attributes = {})
-        @attributes         = attributes
-        @unsaved_attributes = Set.new
+      attr_reader *TOP_LEVEL_DATA, :errors, :changed
+
+      def initialize(data)
+        @id, @type, @attributes, @relationships = data.values_at(*TOP_LEVEL_DATA)
+        @was     = {}
+        @changed = {}
         define_accessors
-        # set_attributes(attributes)
-        # extract_id_from_attributes
       end
 
       def define_accessors
         @attributes.keys.each do |key|
-          self.class.class_eval do
-            define_method(key) do
-              @attributes[key] || @attributes[key.to_sym]
-            end
+          self.singleton_class.class_eval do
+            define_method(key)          { @attributes[key] }
+            define_method("#{key}_was") { @was[key] }
 
             define_method("#{key}=") do |value|
+              @was[key]        = @attributes[key]
               @attributes[key] = value
-              @unsaved_attributes.add(key)
+              @changed.merge!(key => [@was[key], value])
             end
           end
         end
       end
 
-      # def set_attributes(attributes)
-      #   @attributes = attributes
-      #   @attributes.each do |attribute, value|
-      #     add_accessor(attribute)
-      #   end
-      # end
-
-      # def add_accessor(attribute)
-      #   attribute = attribute.to_s
-      #   return if attribute == 'id'
-      #
-      #   singleton_class.class_eval do
-      #     # get
-      #     define_method(attribute) do
-      #       attributes[attribute] || attributes[attribute.to_sym]
-      #     end
-      #
-      #     # set
-      #     define_method("#{attribute}=") do |value|
-      #       self.attributes[attribute] = value
-      #       self.unsaved_attributes.add(attribute)
-      #     end
-      #   end
-      # end
-
-      # def unsaved_attributes
-      #   @unsaved_attributes
-      # end
-      #
-      # def unsaved_data
-      #   @attributes.select { |key| @unsaved_attributes.include?(key) }
-      # end
-
-      # private
-
-      # def extract_id_from_attributes
-      #   @id = @attributes&.delete(:id) || @attributes&.delete('id')
-      # end
-      #
-      # def update_attributes_from_result(result)
-      #   set_attributes(result['attributes'])
-      #   @id = result['id']
-      # end
+      def changes
+        @changed.keys
+      end
     end
   end
 end
