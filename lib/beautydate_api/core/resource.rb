1b4c9c26a4b2551f1761e3eb95ecc4679dd3253e
Tiago Guedes <tiagopog@gmail.com>
2017-08-15 11:25:38 -0300

    Add BeautydateApi::Core::Resource.create

--- lib/beautydate_api/core/resource.rb
+++ lib/beautydate_api/core/resource.rb
@@ -5,18 +5,24 @@ module BeautydateApi
 
       class << self
         def all(params = {})
-          body = call('GET', url, params)
+          body = call('GET', url, params: params)
           body.dig('data').map { |data| new('data' => data, 'included' => body['included']) }
         end
 
         def find(id, params = {})
           raise UnkownIdentifierError, "#{name} ID is unknown" if id.nil?
-          body = call('GET', url(id: id), params)
+          body = call('GET', url(id: id), params: params)
           new(body)
         end
 
-        def call(method, url, params = {})
-          Core::Request.request(method, url, params: params)
+        def create(attributes:, relationships: {})
+          data = normalize_data(attributes, relationships)
+          body = call('POST', url, payload: data)
+          new(body)
+        end
+
+        def call(method, url, params: {}, payload: {})
+          Core::Request.request(method, url, params: params, payload: payload)
         end
 
         def url(id: nil)
@@ -32,6 +38,10 @@ module BeautydateApi
             .parameterize # "business-payments"
         end
 
+        def resource_type
+          resource_path.underscore
+        end
+
         def resourcify!(name)
           BeautydateApi.const_get(
             name            # "business-payments"
@@ -40,6 +50,24 @@ module BeautydateApi
               .camelize     # "BusinessPayment"
           )
         end
+
+        def normalize_data(attributes, relationships)
+          {
+            data: {
+              type: resource_type,
+              attributes: attributes,
+              relationships: normalize_relationships(relationships)
+            }
+          }
+        end
+
+        def normalize_relationships(relationships)
+          relationships.each_with_object({}) do |(key, value), hash|
+            data = { data: { type: key.to_s.pluralize }.merge(value) }
+            hash.merge!(key => data)
+          end
+        end
+
       end
     end
   end
